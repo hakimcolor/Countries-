9addaedae61106a615b8095ce3ff649a6bb4dfec
hakimcolor <hakimcolor777@gmail.com>
2025-09-20 09:49:57 +0600

    my commit

--- src/componentes/Countries/Country.jsx
+++ src/componentes/Countries/Country.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Country = ({ country }) => {
   const countrys = country.name.common;
+  const img = country.flags.flags.png;
+  const alt = country.flags.flags.alt;
+  const countinents = country.continents.continents;
   console.log(countrys);
-  return <div>Name: { countrys}</div>;
+  return (
+    <div >
+
+      <img src={img} alt={alt} />
+      <h1>Name: {countrys}</h1>
+      <h1>Countinents :{ countinents}</h1>
+    </div>
+  );
 };
 
 export default Country;
