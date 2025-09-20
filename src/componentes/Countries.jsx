cc6862869f52eaed1d01aab0a466ef51b1578af5
hakimcolor <hakimcolor777@gmail.com>
2025-09-20 11:32:14 +0600

    my commit

--- src/componentes/Countries.jsx
+++ src/componentes/Countries.jsx
@@ -1,17 +1,31 @@
-import { use } from 'react';
+import { use, useState } from 'react';
 import './Countries.css';
 import Country from './Countries/Country';
 const Countries = ({ countriesPromise }) => {
+  const [count, setCount] = useState([]);
+  const handelcount = (countrys) => {
+    // console.log('hello ');
+    const newvistCntry=[...count,countrys]
+    setCount(newvistCntry);
+    console.log('............. ',countrys);
+    
+  };
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
   // console.log(countries);
   return (
     <div>
       <h2>Countries{countries.length}</h2>
+      <h3>country Visited:{count.length}</h3>
+      {count}
       <div className="countries">
         {' '}
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country}></Country>
+          <Country
+            key={country.ccn3.ccn3}
+            handelcount={handelcount}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
--- src/componentes/Countries/Country.jsx
+++ src/componentes/Countries/Country.jsx
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 import './Country.css';
-const Country = ({ country }) => {
-  const [visited,setVisited]=useState(false)
+const Country = ({ country, handelcount }) => {
+  const [visited, setVisited] = useState(false);
   const hndlClick = () => {
-    console.log('button clicked', countrys);
-    setVisited(!visited)
+    // console.log('button clicked', countrys);
+    setVisited(!visited);
+    handelcount(countrys);
     
-  }
+  };
   const countrys = country.name.common;
   const img = country.flags.flags.png;
   const alt = country.flags.flags.alt;
   const countinents = country.continents.continents;
   const Area = country.area.area;
-  console.log(countrys);
+  // console.log(countrys);
   return (
-    <div className="country">
+    <div className={`country ${visited && 'vist'}`}>
       <img className="flag-img " src={img} alt={alt} />
       <h3>Name: {countrys}</h3>
       <h3>Countinents :{countinents}</h3>
       <h3>
         Area:{Area} {Area > 300000 ? '(big country)' : '(small country)'}
       </h3>
-      <button onClick={hndlClick}> { visited?'Visitead':'Not visited'}</button>
+      <button onClick={hndlClick}>
+        {' '}
+        {visited ? 'Visitead' : 'Not visited'}
+      </button>
     </div>
   );
 };
